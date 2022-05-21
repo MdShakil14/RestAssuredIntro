84dfe5ba0193e073ca174d7d5711a32ed2206dfe
MdShakil14 <mdshakil1420414@gmail.com>
2022-05-21 17:49:29 +0600

    Extent Reports Added.

--- src/test/java/VideoGamesAPITesting/gamesAPITest.java
+++ src/test/java/VideoGamesAPITesting/gamesAPITest.java
@@ -1,28 +1,54 @@
 package VideoGamesAPITesting;
 
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.json.simple.JSONObject;
 import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import static org.hamcrest.Matchers.*;
 
 import javax.annotation.meta.When;
 
+import java.util.PriorityQueue;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 import static io.restassured.RestAssured.*;
 
 public class gamesAPITest {
 
-    @Test
+    ExtentReports reports;
+
+    @BeforeTest
+    public void configReports(){
+        String path =System.getProperty("user.dir")+"\\reports\\index.html";
+        ExtentSparkReporter reporter =new ExtentSparkReporter(path);
+
+        reporter.config().setDocumentTitle("Video Games API Testing");
+        reporter.config().setReportName("API Test");
+
+        reports= new ExtentReports();
+        reports.attachReporter(reporter);
+    }
+
+    @Test(priority = 1)
     public void getVideosGames(){
+
+        reports.createTest("Returns all the videos games in the DB");
         baseURI = "http://localhost:8080/app/";
 
-        when().get("/videogames").then().statusCode(200).log().body();
+        when().get("/videogames").then().statusCode(200);
+        reports.flush();
 
     }
 
-    @Test
+    @Test(priority = 2)
     public void addNewGame(){
+
+        reports.createTest("Add a new video game to the DB");
         baseURI = "http://localhost:8080/app/";
 
         JSONObject jsonObject = new JSONObject();
@@ -40,30 +66,38 @@ public class gamesAPITest {
 
         String jsonString = response.asString();
         Assert.assertTrue(jsonString.contains("Record Added Successfully"));
+        reports.flush();
     }
 
-    @Test
+    @Test(priority = 5)
     public void deleteData(){
+        reports.createTest("Deletes a video game from the DB by ID");
         baseURI = "http://localhost:8080/app/";
 
-        Response response = when().delete("videogames/13")
+        Response response = when().delete("videogames/29")
                 .then().statusCode(200).log().body().extract().response();
 
         String jsonString = response.asString();
         Assert.assertTrue(jsonString.contains("Record Deleted Successfully"));
+        reports.flush();
     }
 
-    @Test
+    @Test(priority = 3)
     public void getSingleVideoGame(){
+
+        reports.createTest("Returns the details of a single game by ID");
+
         baseURI = "http://localhost:8080/app/";
 
         when().get("videogames/14").
                 then().statusCode(200).log().body().
                 body("videoGame.id",equalTo("14"));
+        reports.flush();
     }
 
-    @Test
+    @Test(priority = 4)
     public void editVideoGame(){
+        ExtentTest test = reports.createTest("Update an existing video game in the DB by specifying a new body");
         baseURI = "http://localhost:8080/app/";
 
         JSONObject jsonObject = new JSONObject();
@@ -75,10 +109,14 @@ public class gamesAPITest {
         jsonObject.put("category","Action");
         jsonObject.put("rating", 7.0);
 
-        given().contentType(ContentType.JSON).body(jsonObject.toJSONString())
+        int statusCode =  given().contentType(ContentType.JSON).body(jsonObject.toJSONString())
                 .when().put("videogames/14")
-                .then().statusCode(200).log().body();
+                .then().statusCode(200).extract().statusCode();
+        if(statusCode == 200)
+        {
+            test.fail("Status code doesn't match");
+        }
 
-
+        reports.flush();
     }
 }
